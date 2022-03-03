b5046a6c2f0deb6d910add65131e752becd454f7
Dan <danvmanici@gmail.com>
2022-03-04 00:58:47 +0200

    g

--- src/pages/Rents.js
+++ src/pages/Rents.js
@@ -1,21 +1,135 @@
 import React from "react";
+import "./Sells-Rents.css";
 
 const Rents = () => {
   return (
     <>
-      <div>Rents</div>
-      <section id="residential">
-        Residential
-        <section id="garsoniera">Garsoniera</section>
-        <section id="apartment">Apartment</section>
-        <section id="house">House</section>
-      </section>
-      <section id="residential">
-        Commercial
-        <section id="garsoniera">Garsoniera</section>
-        <section id="apartment">Apartment</section>
-        <section id="house">House</section>
-      </section>
+      <main class="content-main">
+        <article id="residential" class="content-type">
+          <h1>Residential</h1>
+          <section id="garsonierezidential">
+            <h3>Garsoniera</h3>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+          </section>
+          <section id="apartmentrezidential">
+            <h3>Apartment</h3>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+          </section>
+          <section id="houserezidential">
+            <h3>House</h3>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+          </section>
+        </article>
+        <article id="comercial" class="content-type">
+          <h1>Commercial</h1>
+          <section id="garsonieracomerial">
+            <h3>Garsoniera</h3>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+          </section>
+          <section id="apartmentcomerial">
+            <h3>Apartment</h3>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+          </section>
+          <section id="housecomerial">
+            <h3>House</h3>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+            <div>
+              <img
+                src={require("../icons/garsoniera1.jpg")}
+                alt="garsoniera1"
+              />
+              <span>
+                Garsoniera Zona Selgros Se preda cu toate finisajele inclusiv
+                Centrala pe gaz Romstal.
+              </span>
+            </div>
+          </section>
+        </article>
+      </main>
     </>
   );
 };
