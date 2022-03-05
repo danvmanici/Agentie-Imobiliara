9baab403bac6aa1cda0f2c417074f5e9f35a8b41
Dan <danvmanici@gmail.com>
2022-03-05 02:23:36 +0200

    email

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,42 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ContactForm.css";
 import { useTranslation } from "react-i18next";
+import axios from "axios";
 
 const ContactForm = () => {
   const { t } = useTranslation();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [subject, setSubject] = useState("");
+
+  const handleSubmit = async () => {
+    const name = document.querySelector("#name").value;
+    const email = document.querySelector("#email").value;
+    const subject = document.querySelector("#subject").value;
+    await axios.post("http://127.0.0.1:8000/send/", {
+      name: name,
+      email: email,
+      subject: subject,
+    });
+    console.log(name, email, subject);
+  };
+
   return (
     <main>
       <fieldset className="field">
         <legend>{t("contact-us")}</legend>
         <form className="content-field">
           <section>
-            <label for="name">{t("name")}:</label>
+            <label htmlFor="name">{t("name")}:</label>
             <input
               type="text"
               id="name"
               name="name"
               placeholder={t("placeholder-name")}
               required
+              value={name}
+              onChange={(e) => setName(() => e.target.value)}
             />
           </section>
           <section>
-            <label for="email">{t("email")}:</label>
+            <label htmlFor="email">{t("email")}:</label>
             <input
               type="email"
               id="email"
               name="email"
               placeholder={t("placeholder-email")}
               required
+              value={email}
+              onChange={(e) => setEmail(() => e.target.value)}
             />
           </section>
-          <label for="subject"></label>
+          <label htmlFor="subject"></label>
           <textarea
             id="subject"
             name="subject"
             placeholder={t("placeholder-subject")}
             required
+            value={subject}
+            onChange={(e) => setSubject(() => e.target.value)}
           ></textarea>
-          <input type="submit" value={t("submit")} className="send-button" />
+          <input
+            type="submit"
+            value={t("submit")}
+            className="send-button"
+            onClick={(e) => {
+              e.preventDefault();
+              handleSubmit();
+            }}
+          />
         </form>
       </fieldset>
     </main>
